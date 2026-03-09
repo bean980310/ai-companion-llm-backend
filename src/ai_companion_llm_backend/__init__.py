b3250955cf96d17986c6e9f2bd0d15cc67719d12
bean980310 <bean980310@hotmail.co.kr>
2026-03-09 14:46:24 +0900

    Add UnifiedHandlers on __init__.py

--- src/ai_companion_llm_backend/__init__.py
+++ src/ai_companion_llm_backend/__init__.py
@@ -1,6 +1,6 @@
-from .transformers_handlers import TransformersCausalModelHandler, TransformersVisionModelHandler
+from .transformers_handlers import TransformersCausalModelHandler, TransformersVisionModelHandler, TransformersUnifiedModelHandler
 from .gguf_handlers import GGUFCausalModelHandler
-from .mlx_handlers import MlxCausalModelHandler, MlxVisionModelHandler
+from .mlx_handlers import MlxCausalModelHandler, MlxVisionModelHandler, MlxUnifiedModelHandler
 from .vllm_handlers import VllmCausalModelHandler
 from .provider.vllm import vLLMClientWrapper
 # from .langchain_integrator.langchain import LangchainIntegrator
@@ -15,4 +15,4 @@ __all__ = [
     "vLLMClientWrapper",
 ]
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
