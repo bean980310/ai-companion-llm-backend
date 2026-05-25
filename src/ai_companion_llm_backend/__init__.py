975e1cd64a0db4e1e210ad1a12a8b0bd5c3019ca
bean980310 <bean980310@hotmail.co.kr>
2026-05-26 08:12:50 +0900

    Add Provider import on init

--- src/ai_companion_llm_backend/__init__.py
+++ src/ai_companion_llm_backend/__init__.py
@@ -3,6 +3,8 @@ from .gguf_handlers import GGUFCausalModelHandler
 from .mlx_handlers import MlxCausalModelHandler, MlxVisionModelHandler, MlxUnifiedModelHandler
 from .vllm_handlers import VllmCausalModelHandler
 from .provider.vllm import vLLMClientWrapper
+from .provider.lmstudio import LMStudioIntegrator
+from .provider.ollama import OllamaIntegrator
 # from .langchain_integrator.langchain import LangchainIntegrator
 
 __all__ = [
@@ -15,6 +17,8 @@ __all__ = [
     "MlxUnifiedModelHandler",
     "VllmCausalModelHandler",
     "vLLMClientWrapper",
+    "LMStudioIntegrator",
+    "OllamaIntegrator"
 ]
 
 __version__ = "0.5.0"
